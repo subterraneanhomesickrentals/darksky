63162880c62420166f32cc59c6af54cbd9274996
Subterranean Homesick Rentals <46857606+subterraneanhomesickrentals@users.noreply.github.com>
2019-01-30 16:49:32 -0500

    Add Xcode project

---
+++ DarkSky/DarkSky/DarkSky.h
@@ -0,0 +1,12 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DarkSky.
+FOUNDATION_EXPORT double DarkSkyVersionNumber;
+
+//! Project version string for DarkSky.
+FOUNDATION_EXPORT const unsigned char DarkSkyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DarkSky/PublicHeader.h>
+
+
