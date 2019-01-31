76cd5b275caad739b9b611f0934a078db0732b16
Subterranean Homesick Rentals <46857606+subterraneanhomesickrentals@users.noreply.github.com>
2019-01-31 00:35:52 -0500

    Add properties to Response

--- DarkSky/DarkSky/Response.swift
+++ DarkSky/DarkSky/Response.swift
@@ -13,4 +13,22 @@ public struct Response: Codable {
     
     /// DEPRECATED. The current timezone offset in hours. (Use of this property will almost certainly result in Daylight Saving Time bugs. Please use timezone, instead.)
     public let offset: Int?
+    
+    /// A data point containing the current weather conditions at the requested location.
+    public let currently: DataPoint?
+    
+    /// A data block containing the weather conditions minute-by-minute for the next hour.
+    public let minutely: DataBlock?
+    
+    /// A data block containing the weather conditions hour-by-hour for the next two days.
+    public let hourly: DataBlock?
+    
+    /// A data block containing the weather conditions day-by-day for the next week.
+    public let daily: DataBlock?
+    
+    /// An alerts array, which, if present, contains any severe weather alerts pertinent to the requested location.
+    public let alerts: [Alert]?
+    
+    /// A flags object containing miscellaneous metadata about the request.
+    public let flags: Flags?
 }
