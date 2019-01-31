21423440b5b5b30390caf67597f9e163836c0818
Subterranean Homesick Rentals <46857606+subterraneanhomesickrentals@users.noreply.github.com>
2019-01-30 23:52:29 -0500

    Extend Request to compute URL

---
+++ DarkSky/DarkSky/Request+URL.swift
@@ -0,0 +1,34 @@
+
+extension Request {
+    
+    /// The fully formed request URL, including the HTTP query string.
+    public var url: URL? {
+        var components = URLComponents(string: "https://api.darksky.net/forecast/\(key)/\(latitude),\(longitude)")
+        components?.queryItems = queryItems
+        return components?.url
+    }
+    
+    var queryItems: [URLQueryItem]? {
+        
+        var items: [URLQueryItem] = []
+        
+        if let exclude = exclude, exclude.count > 0 {
+            let value = exclude.map { $0.rawValue }.joined(separator: ",")
+            items.append(URLQueryItem(name: "exclude", value: value))
+        }
+        
+        if let extend = extend, extend {
+            items.append(URLQueryItem(name: "extend", value: "hourly"))
+        }
+        
+        if let language = language {
+            items.append(URLQueryItem(name: "lang", value: language.rawValue))
+        }
+        
+        if let units = units {
+            items.append(URLQueryItem(name: "units", value: units.rawValue))
+        }
+        
+        return (items.count > 0) ? items : nil
+    }
+}
