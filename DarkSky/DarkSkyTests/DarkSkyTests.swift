11740811b3d32e341b1b28d1a547e3334f81b821
Subterranean Homesick Rentals <46857606+subterraneanhomesickrentals@users.noreply.github.com>
2019-01-31 23:26:29 -0500

    Add unit test to fetch weather

--- DarkSky/DarkSkyTests/DarkSkyTests.swift
+++ DarkSky/DarkSkyTests/DarkSkyTests.swift
@@ -3,4 +3,32 @@ import XCTest
 @testable import DarkSky
 
 class DarkSkyTests: XCTestCase {
+    
+    static let secretKey = ""
+    
+    override func setUp() {
+        super.setUp()
+        DarkSky.secretKey = DarkSkyTests.secretKey
+    }
+    
+    func testFetchingWeather() {
+        
+        var fetchResult: DarkSky.Result?
+        
+        let expectationa = expectation(description: "testFetchingWeather")
+        
+        DarkSky.weather(latitude: 28.608276, longitude: -80.604097) { result in
+            fetchResult = result
+            expectationa.fulfill()
+        }
+        
+        waitForExpectations(timeout: 60)
+        
+        switch fetchResult! {
+        case .failure(let error):
+            XCTFail("Request failed with error: \(error)")
+        case .success(_):
+            break
+        }
+    }
 }
