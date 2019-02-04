3914164a16da3569e4350787c5363da97528207c
Subterranean Homesick Rentals <46857606+subterraneanhomesickrentals@users.noreply.github.com>
2019-02-03 23:55:19 -0500

    Add enum for Alert severity

--- DarkSky/Alert.swift
+++ DarkSky/Alert.swift
@@ -11,8 +11,8 @@ public struct Alert: Codable {
     /// An array of strings representing the names of the regions covered by this weather alert.
     public let regions: [String]
     
-    /// The severity of the weather alert. Will take one of the following values: "advisory" (an individual should be aware of potentially severe weather), "watch" (an individual should prepare for potentially severe weather), or "warning" (an individual should take immediate action to protect themselves and others from potentially severe weather).
-    public let severity: String
+    /// The severity of the weather alert.
+    public let severity: Severity
     
     /// The UNIX time at which the alert was issued.
     public let time: Date
@@ -22,4 +22,17 @@ public struct Alert: Codable {
     
     /// An HTTP(S) URI that one may refer to for detailed information about the alert.
     public let uri: URL
+    
+    /// The severity of the weather alert.
+    public enum Severity: String, Codable {
+        
+        /// An individual should be aware of potentially severe weather.
+        case advisory
+        
+        /// An individual should prepare for potentially severe weather.
+        case watch
+        
+        /// An individual should take immediate action to protect themselves and others from potentially severe weather.
+        case warning
+    }
 }
