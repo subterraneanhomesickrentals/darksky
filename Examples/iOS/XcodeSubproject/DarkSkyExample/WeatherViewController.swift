5b1f4b0daa113f395fc3960eecdf909cb2cdc274
Subterranean Homesick Rentals <46857606+subterraneanhomesickrentals@users.noreply.github.com>
2019-02-11 13:47:34 -0500

    Update example projects to use new global namespace

--- Examples/iOS/XcodeSubproject/DarkSkyExample/WeatherViewController.swift
+++ Examples/iOS/XcodeSubproject/DarkSkyExample/WeatherViewController.swift
@@ -11,9 +11,9 @@ class WeatherViewController: UIViewController {
         
         label?.text = "Loading..."
         
-        DarkSky.secretKey = ""
+        🌩.secretKey = ""
         
-        DarkSky.weather(latitude: 28.608276, longitude: -80.604097) { result in
+        🌩.weather(latitude: 28.608276, longitude: -80.604097) { result in
             switch result {
             case .success(let weather):
                 self.label?.text = String(format: "%.0f", weather.currently?.temperature ?? 0) + " \(weather.currently?.icon?.emoji ?? "🤷‍♂️")"
