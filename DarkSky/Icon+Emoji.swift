1065f0d402f98dfca54206722ced867f4473b746
Subterranean Homesick Rentals <46857606+subterraneanhomesickrentals@users.noreply.github.com>
2019-02-18 16:10:54 -0500

    Update icon emoji

--- DarkSky/Icon+Emoji.swift
+++ DarkSky/Icon+Emoji.swift
@@ -2,16 +2,16 @@
 extension Icon {
     public var emoji: String {
         switch self {
-        case .clearDay:          return "🌞"
-        case .clearNight:        return "🌜"
+        case .clearDay:          return "☀️"
+        case .clearNight:        return "🌒"
         case .rain:              return "☔️"
         case .snow:              return "❄️"
         case .sleet:             return "🌨"
         case .wind:              return "🌬"
         case .fog:               return "🌁"
         case .cloudy:            return "☁️"
-        case .partlyCloudyDay:   return "🌞☁️"
-        case .partlyCloudyNight: return "🌜☁️"
+        case .partlyCloudyDay:   return "🌤"
+        case .partlyCloudyNight: return "☁️"
         case .undefined:         return "❓"
         }
     }
